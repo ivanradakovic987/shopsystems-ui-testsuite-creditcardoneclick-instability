28368b988d8ab37d3d2de0b24fd2f48df80fca90
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2020-04-23 11:22:14 +0200

    Try filling in country earlier

--- tests/_support/Step/Acceptance/ShopSystem/Magento2Step.php
+++ tests/_support/Step/Acceptance/ShopSystem/Magento2Step.php
@@ -152,7 +152,7 @@ class Magento2Step extends GenericShopSystemStep implements iConfigurePaymentMet
             $this->preparedFillField($this->getLocator()->checkout->last_name, $this->getCustomer($customerType)->getLastName());
             $this->selectOption($this->getLocator()->checkout->country, $this->getCustomer($customerType)->getCountry());
             $this->preparedSelectOption($this->getLocator()->checkout->state, $this->getCustomer($customerType)->getState());
-            $this->wait(5);
+            $this->wait(10);
             $this->fillBillingDetails($customerType);
 
 //            try {
@@ -167,14 +167,14 @@ class Magento2Step extends GenericShopSystemStep implements iConfigurePaymentMet
         //this magento view is very flaky, after the address is filled the shop is loading the delivery options
         // and the button is active or not active at random times, we have to wait to safely click the button
 //        $this->wait(5);
-//        try {
+        try {
             $this->preparedClick($this->getLocator()->checkout->next, 80);
-//        }
-//        catch (UnknownServerException $e)
-//        {
-//            $this->wait(10);
-//            $this->preparedClick($this->getLocator()->checkout->next, 80);
-//        }
+        }
+        catch (UnknownServerException $e)
+        {
+            $this->wait(10);
+            $this->preparedClick($this->getLocator()->checkout->next, 80);
+        }
         $this->waitUntil(60, [$this, 'waitUntilPageLoaded'], [$this->getLocator()->page->payment]);
         $this->wait(3);
     }
